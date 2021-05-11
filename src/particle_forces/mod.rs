b0d388cc650e47f391a9d0e2731fd050d2c81e3b
Henry Sloan <henryksloan@gmail.com>
2021-05-11 13:07:00 -0400

    Add two-particle springs

--- src/particle_forces/mod.rs
+++ src/particle_forces/mod.rs
@@ -2,8 +2,10 @@ pub mod particle_drag;
 pub mod particle_force_generator;
 pub mod particle_force_registry;
 pub mod particle_gravity;
+pub mod particle_spring;
 
 pub use particle_drag::ParticleDrag;
 pub use particle_force_generator::ParticleForceGenerator;
 pub use particle_force_registry::ParticleForceRegistry;
 pub use particle_gravity::ParticleGravity;
+pub use particle_spring::ParticleSpring;
---
+++ src/particle_forces/particle_spring.rs
@@ -0,0 +1,37 @@
+use crate::math::{Real, Vector3};
+use crate::particle::Particle;
+use crate::particle_forces::ParticleForceGenerator;
+
+/// Generates a spring force relative to another particle
+pub struct ParticleSpring {
+    other: Rc<RefCell<Particle>>,
+    spring_constant: Real,
+    rest_length: Real,
+}
+
+impl ParticleForceGenerator for ParticleSpring {
+    fn update_force(&mut self, particle: &mut Particle, _duration: Real) {
+        let difference = particle.get_position() - self.other.borrow_mut().get_position();
+        let direction = {
+            let mut temp = difference;
+            temp.normalize();
+            temp
+        };
+        let distance = difference.magnitude();
+
+        // A spring acts to pull the particle towards the rest length
+        // with force proportional to the spring constant
+        let magnitude = self.spring_constant * (distance - self.rest_length).abs();
+        particle.add_force(direction * -magnitude);
+    }
+}
+
+impl ParticleSpring {
+    pub fn new(other: Rc<RefCell<Particle>>, spring_constant: Real, rest_length: Real) -> Self {
+        Self {
+            other,
+            spring_constant,
+            rest_length,
+        }
+    }
+}
